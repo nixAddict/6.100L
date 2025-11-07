f5db5edf8cb7ecf069ae7a459b302c500e5ef49e
Dang Trung An <nixaddict.dev@gmail.com>
2025-11-07 15:35:21 +0700

    Add initial implementation for Problem Set 1, including test suite and function definitions

---
+++ ps1/ps1a_in_function.py
@@ -0,0 +1,16 @@
+def part_a(yearly_salary, portion_saved, cost_of_dream_home):
+	#########################################################################
+	portion_down_payment = 0.25
+	amount_saved = 0
+	r = 0.05
+	months = 0
+	
+	###############################################################################################
+	## Determine how many months it would take to get the down payment for your dream home below ## 
+	###############################################################################################
+	while amount_saved < cost_of_dream_home * portion_down_payment:
+	    amount_saved += amount_saved * (r / 12) + yearly_salary / 12 * portion_saved
+	    months += 1
+	
+	print(f"Number of months: {months}")
+	return months
