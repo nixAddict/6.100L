4fc810996e21223fb82a4b4869802cdabccda9bc
Dang Trung An <nixaddict.dev@gmail.com>
2025-11-08 20:46:23 +0700

    Add implementation for Problem Set 1, Part C: calculate best savings rate and steps for down payment

--- ps1/ps1c_in_function.py
+++ ps1/ps1c_in_function.py
@@ -1,9 +1,31 @@
 def part_c(initial_deposit):
 	#########################################################################
-	
+	cost_of_dream_home = 800000
+	portion_down_payment = 0.25
+	cost_down_payment = cost_of_dream_home * portion_down_payment
+	months = 36
+	steps = 0
+	epsilon = 100
+	low = 0
+	high = 1
+	r = (high + low) / 2
+	amount_saved = initial_deposit * (1 + high / 12) ** months
 	
 	##################################################################################################
 	## Determine the lowest rate of return needed to get the down payment for your dream home below ##
 	##################################################################################################
+	if amount_saved < cost_down_payment:
+	    r = None
+	else:
+	    while abs(amount_saved - cost_down_payment) > epsilon:
+	        steps += 1
+	        amount_saved = initial_deposit * (1 + r / 12) ** months
+	        if amount_saved < cost_down_payment:
+	            low = r
+	        else:
+	            high = r
+	        r = (high + low) / 2
 	
+	print(f"Best savings rate: {r}")
+	print(f"Steps in bisection search: {steps}")
 	return r, steps
